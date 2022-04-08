984b5676550ac4b538dfa1dae0b71df9e7ff2fa0
Nikita Kochetov <nothsmple@mail.ru>
2022-04-08 21:18:14 +0300

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Татьяна Кочетова on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
