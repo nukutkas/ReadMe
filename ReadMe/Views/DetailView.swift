da789dfb2f2b043842988adcd7e6c02fed778975
Nikita Kochetov <nothsmple@mail.ru>
2022-04-10 16:58:05 +0300

    DetailView has been added.

---
+++ ReadMe/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  ReadMe
+//
+//  Created by Татьяна Кочетова on 10.04.2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        VStack {
+            Book.Image(title: book.title)
+            Spacer()
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(book: .init())
+    }
+}
